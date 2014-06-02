e33a4179056245dac9a517fa28451589f80dafe0
Lagash <lagash@Lagashs-Mac-2.local>
2014-06-02 14:47:52 -0300

    adding Xcode plugin tempalte (SamplePlugin)

---
+++ Samples/SamplePlugin/SamplePlugin/MSSamplePlugin.h
@@ -0,0 +1,13 @@
+//
+//  MSSamplePlugin.h
+//  MSSamplePlugin
+//
+//  Created by Lagash on 6/2/14.
+//  Copyright (c) 2014 Lagash Systems. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface MSSamplePlugin : NSObject
+
+@end
---
+++ Samples/sample.plugin/sample.plugin/MSsample.plugin.m
@@ -0,0 +1,63 @@
+//
+//  MSsample.plugin.m
+//  MSsample.plugin
+//
+//  Created by Lagash on 6/2/14.
+//    Copyright (c) 2014 Lagash Systems. All rights reserved.
+//
+
+#import "MSsample.plugin.h"
+
+static MSsample.plugin *sharedPlugin;
+
+@interface MSsample.plugin()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation MSsample.plugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
