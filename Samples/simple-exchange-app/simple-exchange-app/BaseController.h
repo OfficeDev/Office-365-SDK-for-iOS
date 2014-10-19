000a8385e166a0a82269c9b7a85d8f4ee90637c2
joshgav <josh.gavant@outlook.com>
2014-10-19 15:21:00 -0700

    Modified simple exchange app to use frameworks

--- Samples/simple-exchange-app/simple-exchange-app/BaseController.h
+++ Samples/simple-exchange-app/simple-exchange-app/BaseController.h
@@ -5,10 +5,9 @@
  ******************************************************************************/
 
 #import <Foundation/Foundation.h>
+#import <office365_odata_base/office365_odata_base.h>
 #import <office365_exchange_sdk/MSOEntityContainerClient.h>
-#import <office365_odata_impl/MSODefaultDependencyResolver.h>
-#import <office365_odata_impl/MSOOAuthCredentials.h>
-#import <office365_odata_impl/MSOCredentialsImpl.h>
+
 
 @interface BaseController : NSObject
 
