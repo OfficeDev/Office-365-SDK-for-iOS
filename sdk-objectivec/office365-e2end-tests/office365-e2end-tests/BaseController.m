1ebdd198d557fa3916126cec0195f95fc95d984e
Hansen Gustavo <gustavo.a.hansen@gmail.com>
2014-11-06 17:29:12 -0300

    fixed method name

--- sdk-objectivec/office365-e2end-tests/office365-e2end-tests/BaseController.m
+++ sdk-objectivec/office365-e2end-tests/office365-e2end-tests/BaseController.m
@@ -23,7 +23,7 @@
         [credentialsImpl setCredentials:credentials];
         [resolver setCredentialsFactory:credentialsImpl];
     
-        callback([[MSOutlookClient alloc] initWitUrl:@"https://outlook.office365.com/api/v1.0" dependencyResolver:resolver]);
+        callback([[MSOutlookClient alloc] initWithUrl:@"https://outlook.office365.com/api/v1.0" dependencyResolver:resolver]);
     }];
 }
 @end
