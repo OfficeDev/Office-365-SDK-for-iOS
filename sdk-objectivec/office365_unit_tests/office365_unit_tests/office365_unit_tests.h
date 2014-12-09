1cdd85fb532a6c55f8bbc0230b291c2261cf0ffe
Hansen Gustavo <gustavo.a.hansen@gmail.com>
2014-12-09 11:24:40 -0300

    Added Unit test project

---
+++ sdk-objectivec/office365_unit_tests/office365_unit_tests/office365_unit_tests.h
@@ -0,0 +1,19 @@
+//
+//  office365_unit_tests.h
+//  office365_unit_tests
+//
+//  Created by Gustavo on 12/9/14.
+//  Copyright (c) 2014 MSOpentech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for office365_unit_tests.
+FOUNDATION_EXPORT double office365_unit_testsVersionNumber;
+
+//! Project version string for office365_unit_tests.
+FOUNDATION_EXPORT const unsigned char office365_unit_testsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <office365_unit_tests/PublicHeader.h>
+
+
