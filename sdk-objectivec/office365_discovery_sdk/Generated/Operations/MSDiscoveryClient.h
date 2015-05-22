3b67a35708a899160dd9ab3a93389944a6ffb1d5
Hansen Gustavo <gustavo.a.hansen@gmail.com>
2015-05-22 15:25:23 -0300

    Regenerated Discovery SDK

--- sdk-objectivec/office365_discovery_sdk/Generated/Operations/MSDiscoveryClient.h
+++ sdk-objectivec/office365_discovery_sdk/Generated/Operations/MSDiscoveryClient.h
@@ -14,7 +14,7 @@ the T4TemplateWriter (https://github.com/msopentech/vipr-t4templatewriter).
 
 @class MSDiscoveryServiceInfoCollectionFetcher;
 
-#import <orc_engine_core/orc_engine_core.h>
+#import <core/core.h>
 #import "MSDiscoveryModels.h"
 
 /**
