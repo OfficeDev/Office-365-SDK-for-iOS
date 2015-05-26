d644df3aa3661969a429b2e040a0ec086fb6d8e5
joshgav <josh.gavant@outlook.com>
2015-05-26 12:36:30 -0700

    migrate directory_services

---
+++ sdk-objectivec/directory_services/directory_services/directory_services.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * Licensed under the Apache License, Version 2.0.
+ * See License.txt in the project root for license information.
+ ******************************************************************************/
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double directory_servicesVersionNumber;
+FOUNDATION_EXPORT const unsigned char directory_servicesVersionString[];
+
+#import <directory_services/MSDirectoryModels.h>
+#import <directory_services/MSDirectoryFetchers.h>
