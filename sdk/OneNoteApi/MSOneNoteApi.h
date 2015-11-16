59a66d3a45a5e32f56a1f39643370dc8b22ba0c4
Miguel Ángel Pérez Martínez <zlash@users.noreply.github.com>
2015-11-16 18:22:50 -0300

    Added missing SDKs

---
+++ sdk/OneNoteApi/MSOneNoteApi.h
@@ -0,0 +1,17 @@
+
+
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * Licensed under the Apache License, Version 2.0.
+ * See License.txt in the project root for license information.
+ ******************************************************************************/
+
+#ifndef MSONENOTEAPI_H
+#define MSONENOTEAPI_H
+
+#import "MSOneNoteApiFetchers.h"
+#import "MSOneNoteApiModels.h"
+
+#endif
+    
